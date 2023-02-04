5fd103e6eefa312716ed52c0cb47ced4a5defa9a
mocatfrio <hafarafirdausi@gmail.com>
2023-02-05 00:35:36 +0700

    [UI] Add image for WA sharing

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -7,6 +7,9 @@
     <meta name="csrf-token" value="{{ csrf_token() }}" />
     <title>F&F @yield('title')</title>
     <link rel="icon" type="image/x-icon" href="{{ asset('images/favicon.ico') }}">
+    <meta property="og:image" content="{{ asset('images/logo.png') }}" />
+    <meta property="og:url" content="https://ffforever.fun/" />
+    <meta property="og:title" content="F&F Wedding Invitation" />
 
     <!-- Fonts  -->
     <link rel="stylesheet" href="{{ asset('vendor/font-awesome/css/fontawesome.min.css') }}">
